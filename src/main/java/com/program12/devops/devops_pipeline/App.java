dfd39616bab26e7464e35659fe88aa5738a844c6
Shradha Ajit Shanbhag <shradhashanbhag777@gmail.com>
2026-04-21 10:07:48 +0530

    my devops project

---
+++ src/main/java/com/program12/devops/devops_pipeline/App.java
@@ -0,0 +1,13 @@
+package com.program12.devops.devops_pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
